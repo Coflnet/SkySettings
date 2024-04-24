4486a9d25ef4c383abf3646b8092f8e1721a4f60
Äkwav <ekwav@coflnet.com>
2024-04-24 14:08:54 +0200

    make client target .net 6

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,9 +1,9 @@
-VERSION="0.2.1"
+VERSION="0.2.2"
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5004/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Settings.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=Coflnet.Sky.Settings.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
 sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Sky.Settings.Client/Coflnet.Sky.Settings.Client.csproj
@@ -11,4 +11,4 @@ sed -i 's/GIT_REPO_ID/SkySettings/g' src/Coflnet.Sky.Settings.Client/Coflnet.Sky
 sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Sky.Settings.Client/Coflnet.Sky.Settings.Client.csproj
 
 dotnet pack
-cp src/Coflnet.Sky.Settings.Client/bin/Debug/Coflnet.Sky.Settings.Client.*.nupkg ..
+cp src/Coflnet.Sky.Settings.Client/bin/Release/Coflnet.Sky.Settings.Client.*.nupkg ..
